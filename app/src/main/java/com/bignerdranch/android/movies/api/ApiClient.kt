0080d743ebcf7dfc7fd604e19689078e9e903ca0
yui <yuineor@gmail.com>
2024-12-27 19:42:03 +0700

    get api key and add this

---
+++ app/src/main/java/com/bignerdranch/android/movies/api/ApiClient.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.movies.api
+
+
+
+object ApiClient {
+    private const val BASE_URL = "https://www.omdbapi.com/"
+    private const val API_KEY = "c2493de3"
+
+
+}
