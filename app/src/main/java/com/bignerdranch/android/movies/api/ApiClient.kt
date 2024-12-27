38e22d8eaa6a22ca5b6c025fdb329a11caaa1edf
yui <yuineor@gmail.com>
2024-12-27 19:44:27 +0700

    connect api client with api interceptor

--- app/src/main/java/com/bignerdranch/android/movies/api/ApiClient.kt
+++ app/src/main/java/com/bignerdranch/android/movies/api/ApiClient.kt
@@ -1,10 +1,16 @@
 package com.bignerdranch.android.movies.api
 
-
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     private const val BASE_URL = "https://www.omdbapi.com/"
     private const val API_KEY = "c2493de3"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(ApiInterceptor(API_KEY))
+        .build()
 
 
 }
